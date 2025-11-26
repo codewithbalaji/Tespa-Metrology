ab36802b4e30a9580a37416c31bd29b279c1b077
balaji <balaji01975@gmail.com>
2025-11-26 12:35:02 +0530

    refactor(admin): remove unused currency import from List component
    - Remove unused `currency` import from App module in List.tsx
    - Simplify imports to only include necessary dependencies
    - Reduce code clutter and improve maintainability

--- admin/src/pages/List.tsx
+++ admin/src/pages/List.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { backendUrl, currency } from '../App'
+import { backendUrl} from '../App'
 import { toast } from 'react-toastify'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
